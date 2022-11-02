ae6896a1b1a10f72149f36c6c23070f9fd750fcc
Hiromi Furukawa <umi8@users.noreply.github.com>
2022-11-02 20:36:36 +0900

    feat: read line by line and println

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
+
+use anyhow::Result;
+use clap::Parser;
+
+/// Jack Compiler
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Sets a file path to be compiled
+    #[arg(value_name = "PATH")]
+    path: PathBuf,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let file = File::open(args.path.as_path())?;
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        println!("{}", line?);
+    }
+
+    Ok(())
 }

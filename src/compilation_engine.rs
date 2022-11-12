0f753a6c9ca57599dd5fc3e6420b6402064b662c
Hiromi Furukawa <umi8@users.noreply.github.com>
2022-11-12 17:31:14 +0900

    feat: write to vec or stdout

--- src/compilation_engine.rs
+++ src/compilation_engine.rs
@@ -1,67 +1,75 @@
+use std::io::Write;
+
 use anyhow::{bail, Error, Result};
 
 use crate::{JackTokenizer, TokenType};
 
 trait CompilationEngine {
-    fn new(tokenizer: JackTokenizer) -> Self;
+    fn new(tokenizer: JackTokenizer, writer: Box<dyn Write>) -> Self;
     fn compile_class(&mut self) -> Result<()>;
 }
 
 struct XmlCompilationEngine {
     tokenizer: JackTokenizer,
+    writer: Box<dyn Write>,
 }
 
 impl CompilationEngine for XmlCompilationEngine {
-    fn new(tokenizer: JackTokenizer) -> Self {
-        XmlCompilationEngine { tokenizer }
+    fn new(tokenizer: JackTokenizer, writer: Box<dyn Write>) -> Self {
+        XmlCompilationEngine { tokenizer, writer }
     }
 
     fn compile_class(&mut self) -> Result<()> {
-        println!("<class>");
+        writeln!(self.writer, "<class>")?;
 
         self.tokenizer.advance()?;
         match self.tokenizer.token_type() {
-            TokenType::Keyword => {
-                println!(
-                    "<keyword> {} </keyword>",
-                    self.tokenizer.key_word()?.to_string().to_lowercase()
-                )
-            }
+            TokenType::Keyword => writeln!(
+                self.writer,
+                "<keyword> {} </keyword>",
+                self.tokenizer.key_word()?.to_string().to_lowercase()
+            )?,
             _ => bail!(Error::msg("Illegal token")),
         }
 
         self.tokenizer.advance()?;
         match self.tokenizer.token_type() {
-            TokenType::Identifier => {
-                println!("<identifier> {} </identifier>", self.tokenizer.identifier())
-            }
+            TokenType::Identifier => writeln!(
+                self.writer,
+                "<identifier> {} </identifier>",
+                self.tokenizer.identifier()
+            )?,
             _ => bail!(Error::msg("Illegal token")),
         }
 
         self.tokenizer.advance()?;
         match self.tokenizer.token_type() {
-            TokenType::Symbol => {
-                println!("<symbol> {} </symbol>", self.tokenizer.symbol())
-            }
+            TokenType::Symbol => writeln!(
+                self.writer,
+                "<symbol> {} </symbol>",
+                self.tokenizer.symbol()
+            )?,
             _ => bail!(Error::msg("Illegal token")),
         }
 
         self.tokenizer.advance()?;
         match self.tokenizer.token_type() {
-            TokenType::Symbol => {
-                println!("<symbol> {} </symbol>", self.tokenizer.symbol())
-            }
+            TokenType::Symbol => writeln!(
+                self.writer,
+                "<symbol> {} </symbol>",
+                self.tokenizer.symbol()
+            )?,
             _ => bail!(Error::msg("Illegal token")),
         }
 
-        println!("</class>");
+        writeln!(self.writer, "</class>")?;
         Ok(())
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::io::{Seek, SeekFrom, Write};
+    use std::io::{stdout, Seek, SeekFrom, Write};
 
     use crate::compilation_engine::{CompilationEngine, XmlCompilationEngine};
     use crate::JackTokenizer;
@@ -73,7 +81,9 @@ mod tests {
         writeln!(file, "}}").unwrap();
         file.seek(SeekFrom::Start(0)).unwrap();
         let tokenizer = JackTokenizer::new(file).unwrap();
-        let mut engine = XmlCompilationEngine::new(tokenizer);
+        let buf = Vec::<u8>::new();
+        let buf = stdout().lock();
+        let mut engine = XmlCompilationEngine::new(tokenizer, Box::new(buf));
 
         engine.compile_class().unwrap();
 
